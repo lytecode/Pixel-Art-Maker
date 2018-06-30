154a108558473d91888baed300a76f7a1e16e8d5
Basil <mbonubasil@gmail.com>
2018-06-30 16:11:09 +0100

    add jQuery to the index page and can now get the values of width and height from the form

--- designs.js
+++ designs.js
@@ -1,10 +1,24 @@
 // Select color input
 // Select size input
-
+let inputHeight;
+let inputWidth;
+let color;
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').on('submit', function(evt){
+   
+    inputHeight = $('#inputHeight').val();
+    inputWidth = $('#inputWeight').val();
+    console.log('inputHeight: ', inputHeight);
+    console.log('inputWeight: ', inputWidth);
+    
+    makeGrid();
+    evt.preventDefault();
+});
 
 function makeGrid() {
 
 // Your code goes here!
+
+console.log('inputHeight 2: ', inputHeight);
 
 }
