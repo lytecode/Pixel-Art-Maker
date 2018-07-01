cb74a77b4d08f57c81d53f39c0832ddd065bf5a5
Basil <mbonubasil@gmail.com>
2018-07-01 01:58:22 +0100

    Cells can be colored based on color selected

--- designs.js
+++ designs.js
@@ -3,28 +3,29 @@
 let inputHeight, inputWidth, color;
 
 // When size is submitted by the user, call makeGrid()
-$('#sizePicker').on('submit', function(evt){
-   
+$('#sizePicker').on('submit', function(evt){ 
     inputHeight = $('#inputHeight').val();
     inputWidth = $('#inputWeight').val();
-    console.log('inputHeight: ', inputHeight);
-    console.log('inputWeight: ', inputWidth);
     
     $('#pixelCanvas > tr').remove();
     makeGrid();
     evt.preventDefault();
 });
 
-function makeGrid() {
 
+function makeGrid() {
 // Your code goes here!
     for(let row = 1; row <= inputWidth; row++){
         $('#pixelCanvas').append('<tr></tr>');
         for(let col = 1; col <= inputHeight; col++){
             $('tr:last').append('<td></td>');
+            $('td').addClass('cell');
         }
-
     }
 
-
+    
+    $('.cell').click(function(event){
+        color = $('#colorPicker').val()
+        $(event.target).css('background-color', color);
+    })
 }
